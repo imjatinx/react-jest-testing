11c5965771c179c53ecb94f7bb400afc5b26e7ce
Jatin G <45586126+imjatinx@users.noreply.github.com>
2024-07-06 16:26:03 +0530

    form input test cases

--- src/App.js
+++ src/App.js
@@ -9,8 +9,16 @@ function App() {
         <h1>Hello world learn react</h1>
         <input
           type="text"
+          name="full name"
+          placeholder='Enter your full name' />
+        <input
+          type="text"
           name="username"
           placeholder='Enter user name' />
+        <input
+          type="password"
+          name="password"
+          placeholder='Enter your password' />
       </div>
     </>
   );
--- src/App.test.js
+++ src/App.test.js
@@ -1,34 +1,37 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+// Individual test case
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
 
-
-test("test input", () => {
-  render(<App />);
-  const text = screen.getByRole("textbox")
-  const textByPlaceholder = screen.getByPlaceholderText("Enter user name")
-
-  expect(text).toBeInTheDocument();
-  expect(textByPlaceholder).toBeInTheDocument();
-  expect(text).toHaveAttribute("name", "username")
-  expect(text).toHaveAttribute("placeholder", "Enter user name")
-})
-
-describe.skip('UI Group 1', () => {
-  test("ui test 1", () => {
+// Group test case for form element/inputs
+describe('UI Group 1', () => {
+  test("ui test for full name", () => {
     render(<App />);
-    const linkElement = screen.getByText(/learn react/i);
-    expect(linkElement).toBeInTheDocument();
+    const fullNameInput = screen.getByPlaceholderText('Enter your full name');
+
+    expect(fullNameInput).toBeInTheDocument();
+    expect(fullNameInput).toHaveAttribute('name', 'full name');
+    expect(fullNameInput).toHaveAttribute('placeholder', 'Enter your full name');
   })
-  test("ui test 2", () => {
+  test("test username input", () => {
     render(<App />);
-    const linkElement = screen.getByText(/learn react/i);
-    expect(linkElement).toBeInTheDocument();
+    const usernameInput = screen.getByPlaceholderText("Enter user name")
+
+    expect(usernameInput).toBeInTheDocument();
+    expect(usernameInput).toHaveAttribute("name", "username")
+    expect(usernameInput).toHaveAttribute("placeholder", "Enter user name")
+  })
+  test("ui test for password", () => {
+    render(<App />);
+    const fullNameInput = screen.getByPlaceholderText('Enter your password');
+    expect(fullNameInput).toBeInTheDocument();
+    expect(fullNameInput).toHaveAttribute('name', 'password');
+    expect(fullNameInput).toHaveAttribute('placeholder', 'Enter your password');
   })
 
 })
