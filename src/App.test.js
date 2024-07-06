5b7a5835f59ea21f67049b025af4298dc4f2591c
Jatin G <45586126+imjatinx@users.noreply.github.com>
2024-07-06 14:50:11 +0530

    Jest test cases, describe

--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,51 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+
+test("test input", () => {
+  render(<App />);
+  const text = screen.getByRole("textbox")
+  const textByPlaceholder = screen.getByPlaceholderText("Enter user name")
+
+  expect(text).toBeInTheDocument();
+  expect(textByPlaceholder).toBeInTheDocument();
+  expect(text).toHaveAttribute("name", "username")
+  expect(text).toHaveAttribute("placeholder", "Enter user name")
+})
+
+describe.skip('UI Group 1', () => {
+  test("ui test 1", () => {
+    render(<App />);
+    const linkElement = screen.getByText(/learn react/i);
+    expect(linkElement).toBeInTheDocument();
+  })
+  test("ui test 2", () => {
+    render(<App />);
+    const linkElement = screen.getByText(/learn react/i);
+    expect(linkElement).toBeInTheDocument();
+  })
+
+})
+
+describe('API Group 1', () => {
+  test("api test 1", () => {
+    render(<App />);
+    const linkElement = screen.getByText(/learn react/i);
+    expect(linkElement).toBeInTheDocument();
+  })
+  test("api test 2", () => {
+    render(<App />);
+    const linkElement = screen.getByText(/learn react/i);
+    expect(linkElement).toBeInTheDocument();
+  })
+
+  describe('sub api testing 1', () => {
+    test("api test 2", () => {
+      render(<App />);
+      const linkElement = screen.getByText(/learn react/i);
+      expect(linkElement).toBeInTheDocument();
+    })
+  })
+
+})
